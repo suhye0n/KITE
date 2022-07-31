6b31c4583f95fc89a1442cdb5b3d35bc0d2aee88
suhye0n <contact.suhyeon@gmail.com>
2022-07-31 21:20:08 +0900

    220731

---
+++ kite/src/Main.js
@@ -0,0 +1,22 @@
+import './Main.css';
+
+function Main() {
+  return (
+    <div className="Main">
+      <header className="Main-header">
+        <a
+          className="App-link"
+          href="/"
+          target="_top"
+          rel="noopener noreferrer"
+        >
+        <p>
+          KITE
+        </p>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Main;

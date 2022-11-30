a2e9bd44f03e8934d341798f08306d1201c92daa
suhye0n <contact.suhyeon@gmail.com>
2022-11-30 22:49:36 +0900

    지도 크기 수정

--- src/component/Officer.js
+++ src/component/Officer.js
@@ -29,6 +29,20 @@ export function Officer() {
       <h3>회장</h3>
       <p>박도현</p>
       <br />
+      <h3>부회장</h3>
+      <p>Normatov Sodikov</p>
+      <br />
+      <h3>총무</h3>
+      <p>이동혁</p>
+      <br />
+      <h3>기획부장</h3>
+      <p>설재웅</p>
+      <br />
+      <h3>국제교류부장</h3>
+      <p>문종훈</p>
+      <br />
+      <h3>학습부장</h3>
+      <p>강예린</p>
     </>
   );
 }
